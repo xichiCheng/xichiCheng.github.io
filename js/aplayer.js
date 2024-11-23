f688ff8bfea8a3da6ae04788c938b8015111805b
xichiCheng <a15879224140@out.look>
2024-11-23 11:30:54 +0800

    Site updated: 2024-11-23 11:30:48

--- js/aplayer.js
+++ js/aplayer.js
@@ -6,18 +6,61 @@
         new APlayer({
             container: document.getElementById('aplayer'),
             fixed: true,
-            autoplay: false,
+            autoplay: true,
             loop: 'all',
             order: 'random',
             theme: '#b7daff',
             preload: 'none',
             audio: [
                 {
-                    name: 'song1',
-                    artist: 'artist1',
+                    name: 'Collapsing World',
+                    artist: 'Lightscape',
                     url: '/songs/Lightscape - Collapsing World.flac',
                     cover: '/img/img30.jpg'
+                },
+                {
+                    name: 'DUVET',
+                    artist: 'bôa',
+                    url: '/songs/bôa - DUVET.flac',
+                    cover: '/img/img30.jpg'
+                },
+                {
+                    name: 'So Far Away',
+                    artist: 'Martin Garrix',
+                    url: '/songs/Martin Garrix - So Far Away.flac',
+                    cover: '/img/img30.jpg'
+                },
+                {
+                    name: 'Dehors',
+                    artist: 'Jordann',
+                    url: '/songs/Jordann - Dehors.flac',
+                    cover: '/img/img30.jpg'
+                },
+                {
+                    name: 'So Far Away (Acoustic)',
+                    artist: 'Adam Christopher',
+                    url: '/songs/Adam Christopher - So Far Away (Acoustic).flac',
+                    cover: '/img/img30.jpg'
+                },
+                {
+                    name: 'Towards The Light',
+                    artist: 'Jacoo',
+                    url: '/songs/Jacoo - Towards The Light.flac',
+                    cover: '/img/img30.jpg'
+                },
+                {
+                    name: 'Daylight',
+                    artist: 'Seredris',
+                    url: '/songs/Seredris - Daylight.flac',
+                    cover: '/img/img30.jpg'
+                },
+                {
+                    name: 'Dusk Till Dawn',
+                    artist: '晴诚',
+                    url: '/songs/晴诚 - Dusk Till Dawn.flac',
+                    cover: '/img/img30.jpg'
                 }
+
             ]
         });
     }
